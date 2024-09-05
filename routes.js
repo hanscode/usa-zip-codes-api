e3e2f65d73666935d22192fa2cfc230906f54837
Hans Steffens <hans@steffens.co>
2024-09-05 06:32:21 -0500

    Get all zip codes for a specific state or city

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const express = require("express");
 // Construct a router instance.
 const router = express.Router();
 const ZipCode = require("./models").ZipCode;
+const { Op } = require("sequelize");
 
 /**
  * Handler function to wrap each route with. Reduces try.. catch blocks.
@@ -34,6 +35,54 @@ router.get(
       res.json(zipCode);
     } else {
       res.status(404).json({ message: "Zip code not found." });
+    }
+  })
+);
+
+// Get all zip codes for a specific state with pagination
+router.get(
+  "/state/:state",
+  asyncHandler(async (req, res) => {
+    const state = req.params.state.toUpperCase();
+    const limit = req.query.limit || 10;
+    const offset = req.query.offset || 0;
+    const zipCodes = await ZipCode.findAndCountAll({
+      where: { 
+        state: {
+          [Op.like]: state,
+        },
+       },
+      limit,
+      offset,
+    });
+    if (zipCodes.count > 0) {
+      res.json(zipCodes);
+    } else {
+      res.status(404).json({ message: "No zip codes found for this state." });
+    }
+  })
+);
+
+// Get all zip codes for a specific city with pagination
+router.get(
+  "/city/:city",
+  asyncHandler(async (req, res) => {
+    const city = req.params.city.replace(/\+/g, " ");
+    const limit = req.query.limit || 10;
+    const offset = req.query.offset || 0;
+    const zipCodes = await ZipCode.findAndCountAll({
+      where: { 
+        city: {
+          [Op.like]: city,
+        },
+       },
+      limit,
+      offset,
+    });
+    if (zipCodes.count > 0) {
+      res.json(zipCodes);
+    } else {
+      res.status(404).json({ message: "No zip codes found for this city." });
     }
   })
 );
